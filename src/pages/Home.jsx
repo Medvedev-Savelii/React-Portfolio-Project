49da5b7c971ddb86ce8d832321b32564d93543cf
sava <savamedvedev@gmail.com>
2022-01-23 03:24:56 +0800

    fourt commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeroSections from "../components/HeroSections";
 import AboutSection from "../components/AboutSection";
 import ServiceSection from "../components/ServiceSection";
+import ProjectsSection from "../components/ProjectsSection";
 export default function Home() {
   return (
     <div>
       <HeroSections />
       <AboutSection />
       <ServiceSection />
+      <ProjectsSection />
     </div>
   );
 }
